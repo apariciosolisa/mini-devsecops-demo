a7aae65a43543e606ce06f05c1c33a3059f2ad22
Anthony Aparicio <apariciosolisa@gmail.com>
2025-05-21 15:56:52 -0600

    Versión para ver resultados del pipeline

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hola desde DevSecOps en Azure Prueba de escaneos"
+    return "Hola desde DevSecOps en Azure. Resultados del pipeline"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
